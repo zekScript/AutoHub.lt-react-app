4773cd0c89f963170ba8ccb34c1aa52a5fa13ccd
zekScript <armandasdarbinis@gmail.com>
2025-11-24 20:46:46 +0100

    h

--- client/src/pages/user/CarListings.jsx
+++ client/src/pages/user/CarListings.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
-import './CarListing.css'
+import './carListing.css'
 import formatDate from '../../components/funcs/formatDate'
 import { numberWithCommas } from '../../components/funcs/bigNumberSeparation'
 import engineConverter from "../../components/funcs/horsepowerAndKWConverter"
